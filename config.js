6555acd387dba6d04a8c0be8c5c111ce6a733b8e
Atomm94 <atom.makaryan.94@gmail.com>
2020-09-14 10:58:59 +0400

    new dashboard update

--- config.js
+++ config.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
-mongoose.connect(process.env.DATABASE, {
+const config = {
+    PORT:3000,
+    DATABASE:'mongodb+srv://At11:atmak11@cluster0.d1re6.mongodb.net/googleDashboard?retryWrites=true&w=majority',
+    GOOGLE_CLIENT_ID: '385039367681-4qh2mpp5g5m7qi6ruf26jdg9vt4ef6h4.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: 'r-BO-6fStFfiwaBijfy9sI9o',
+    GOOGLE_CALLBACK_URL: 'http://localhost:3000/google/callback',
+    API_KEY: 'AIzaSyC5SMwPoMtEkG2EdG8nEtwVXnVrTDg6k3A'
+}
+
+
+mongoose.connect(config.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -19,14 +29,6 @@ const authorization = function (req, res, next) {
     });
 }
 
-const config = {
-    PORT:3000,
-    DATABASE:'mongodb://localhost:27017/spreadsheetsDB',
-    GOOGLE_CLIENT_ID: '385039367681-4qh2mpp5g5m7qi6ruf26jdg9vt4ef6h4.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: 'r-BO-6fStFfiwaBijfy9sI9o',
-    GOOGLE_CALLBACK_URL: 'http://localhost:3000/google/callback',
-    API_KEY: 'AIzaSyC5SMwPoMtEkG2EdG8nEtwVXnVrTDg6k3A'
-}
 
 module.exports = {
     authorization,
